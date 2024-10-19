715303200a6b9480a60c016bf38f0784e9b621cc
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-10-19 08:26:01 +0500

    added separate job pages

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -32,7 +32,7 @@ const JobListing = ({job}) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <FaMapMarker className='inline text-lg mb-1 mr-2 '/>
+            <FaMapMarker className='inline text-lg mb-2 mr-2 '/>
             {job.location}
           </div>
           <Link
