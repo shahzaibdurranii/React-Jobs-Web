52c6ed3be12d8cc1be83430f49c43ad53837743b
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-10-19 10:22:37 +0500

    Added the 'Add New Job ' functionality

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -10,9 +10,7 @@ const JobPage = () => {
     <>
       <section>
       <div className="container m-auto py-6 px-6">
-        <Link
-          to="/jobs"
-          className="text-indigo-500 hover:text-indigo-600 flex items-center">
+        <Link to="/jobs" className="text-indigo-500 hover:text-indigo-600 flex items-center">
           <FaArrowLeft className='mr-2'/> Back to Job Listings
         </Link>
       </div>
@@ -23,31 +21,30 @@ const JobPage = () => {
         <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
           <main>
             <div
-              className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
-            >
-              <div className="text-gray-500 mb-4">Full-Time</div>
+              className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
+              <div className="text-gray-500 mb-4">{job.type}</div>
               <h1 className="text-3xl font-bold mb-4">
-                Senior React Developer
+                {job.title}
               </h1>
               <div
                 className="text-orange-700 mb-4 flex align-middle justify-center md:justify-start">
                 <FaMapMarker className='mr-1 '/>
-                <p className="text-orange-700">Boston, MA</p>
+                <p className="text-orange-700">{job.location}</p>
               </div>
             </div>
 
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 className="text-indigo-800 text-lg font-bold mb-6">
-                Job Description
+                Job description
               </h3>
 
               <p className="mb-4">
-               We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript, with experience working with modern JavaScript frameworks such as React or Angular.
+                {job.description}              
               </p>
 
               <h3 className="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
 
-              <p className="mb-4">$70k - $80K / Year</p>
+              <p className="mb-4">{job.salary}/ Year</p>
             </div>
           </main>
 
@@ -57,10 +54,10 @@ const JobPage = () => {
             <div className="bg-white p-6 rounded-lg shadow-md">
               <h3 className="text-xl font-bold mb-6">Company Info</h3>
 
-              <h2 className="text-2xl">NewTek Solutions</h2>
+              <h2 className="text-2xl">{job.company.name}</h2>
 
               <p className="my-2">
-                NewTek Solutions is a leading technology company specializing in web development and digital solutions. We pride ourselves on delivering high-quality products and services to our clients while fostering a collaborative and innovative work environment.
+                {job.company.description}              
               </p>
 
               <hr className="my-4" />
@@ -68,25 +65,25 @@ const JobPage = () => {
               <h3 className="text-xl">Contact Email:</h3>
 
               <p className="my-2 bg-indigo-100 p-2 font-bold">
-                contact@newteksolutions.com
+                {job.company.contactEmail}
               </p>
 
               <h3 className="text-xl">Contact Phone:</h3>
 
-              <p className="my-2 bg-indigo-100 p-2 font-bold">555-555-5555</p>
+              <p className="my-2 bg-indigo-100 p-2 font-bold">{job.company.contactPhone}</p>
             </div>
 
             {/* <!-- Manage --> */}
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 className="text-xl font-bold mb-6">Manage Job</h3>
               <Link
-                to="/add-job"
-                className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                to={`/jobs/edit/${job.id}`}
+                className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold 
+                py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job
               </Link>
-              <button
-                className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-              >
+              <button className="bg-red-500 hover:bg-red-600 text-white 
+                font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                 Delete Job
               </button>
             </div>
