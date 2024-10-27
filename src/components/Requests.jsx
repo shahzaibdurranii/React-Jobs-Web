81115d80994b6081f7ac00cfcbef3f76b5bfe54e
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-10-27 12:42:34 +0500

    resolving the issue

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,8 +1,8 @@
-import {API_URL} from '../config'
+import {API_URL} from '../config';
 
 // Add Job
 export const addJob = async (newJob) => {
-  const res = await fetch('https://my-json-server.typicode.com/shahzaibdurranii/React-Jobs-Web/jobs', {
+  const res = await fetch(`${API_URL}/jobs`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
