e17017115240a7fe009dc5b24a3271cb28204102
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-10-27 14:31:57 +0500

    same

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -2,7 +2,7 @@ import {API_URL} from '../config';
 
 // Add Job
 export const addJob = async (newJob) => {
-  const res = await fetch('https://my-json-server.typicode.com/shahzaibdurranii/React-Jobs-Web/jobs', {
+  const res = await fetch(`${API_URL}/jobs`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -15,7 +15,7 @@ export const addJob = async (newJob) => {
 
 // Delete Job
 export const deleteJob = async (id) => {
-  const res = await fetch(`https://my-json-server.typicode.com/shahzaibdurranii/React-Jobs-Web/jobs/${id}`, {
+  const res = await fetch(`${API_URL}/jobs/${id}`, {
     method: 'DELETE',
   });
   return res.json(); // Return response data if needed
@@ -23,7 +23,7 @@ export const deleteJob = async (id) => {
 
 // Update Job
 export const updateJob = async (job) => {
-  const res = await fetch(`https://my-json-server.typicode.com/shahzaibdurranii/React-Jobs-Web/jobs/${job.id}`, {
+  const res = await fetch(`${API_URL}/jobs/${job.id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
